286d1c4d8ef1709656528c5511d3136daf131f0c
Arthur S <arthurazevedods@gmail.com>
2020-06-29 16:33:48 -0300

    edit 1136

--- 1136.py
+++ 1136.py
@@ -1,10 +1,11 @@
 def bingo(N,B,bolas):
-	teste = set()
+	teste = set() #coleção que guardará os números de 0 a N
+	#laço para fazer os calculos entre os números
 	for i in range(B):
 		for j in range(i,B):
-			ball = abs(bolas[i]-bolas[j])
+			ball = abs(bolas[i]-bolas[j]) #valor absoluto, se dê negativo, serve para o positivo
 			teste.add(ball)
-			if(len(teste)>=N+1):
+			if(len(teste)==N+1): #se for igual N, então quer dizer tem a possibilidade de sair todos os números de 0 a N 
 				return 'Y'
 	return 'N'
 
@@ -12,11 +13,9 @@ def bingo(N,B,bolas):
 run = True
 while run == True:
 	entrada = input()
-	N,B = (int(n) for n in entrada.split(" ") )
-	if(N == 0 and B == 0):
+	N,B = (int(n) for n in entrada.split(" ") ) #receber a entrada do N e do B
+	if(N == 0 and B == 0): 
 		run =  False
 	else:
-		bolas = [int(b) for b in input().split()]
+		bolas = [int(b) for b in input().split()] #recebe as bolas que ficarão no bingo
 		
-		print(bingo(N,B,bolas))
-		#bingo(N,B,bolas)
